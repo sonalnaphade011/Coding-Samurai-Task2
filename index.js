04db92f58aafebc9ffb27bdf3ac66e88218e7536
sonalnaphade001 <sonalnaphade2001@gmail.com>
2023-11-22 20:06:52 +0530

    add new

---
+++ index.js
@@ -0,0 +1,54 @@
+// preloader
+var loader = document.getElementById("loading");
+
+ window.addEventListener("load", function(){
+   loader.style.display = "none";
+})
+
+// To Display Time 
+let time = document.getElementById("time");
+
+setInterval(() =>{
+    let d = new Date();
+time.innerHTML = d.toLocaleTimeString();
+},1000)
+
+// To Add Tasks in To Do List
+const input = document.querySelector('input');
+const btn = document.querySelector('.AddTask > button');
+btn.addEventListener('click', AddList);
+input.addEventListener('keyup', (e)=>{
+    (e.keyCode === 13 ? AddList(e) : null);
+})
+
+function AddList(e){
+    const InCompleted = document.querySelector('.InCompleted');
+    const Completed = document.querySelector('.Completed');
+
+    const NewLi = document.createElement('li');
+    const CheckBtn = document.createElement('button');
+    const DelBtn = document.createElement('button');
+    
+    CheckBtn.innerHTML = '<i class="fa-solid fa-square-check"></i>';
+    DelBtn.innerHTML = '<i class="fa-solid fa-square-xmark"></i>';
+
+
+    if(input.value !==''){
+        NewLi.textContent = input.value;
+        input.value = '';
+        InCompleted.appendChild(NewLi);
+        NewLi.appendChild(CheckBtn);
+        NewLi.appendChild(DelBtn);                    
+    }
+
+    CheckBtn.addEventListener('click', function(){
+        const parent = this.parentNode;
+        parent.remove();
+        Completed.appendChild(parent);
+        CheckBtn.style.display = 'none';
+    });
+    DelBtn.addEventListener('click', function(){
+        const parent = this.parentNode;
+        parent.remove();
+    });
+}
